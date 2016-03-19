1e3e9c5883b54aa3891220e4b1302487cc647ac5
shanibilu <shanibilu92@gmail.com>
2016-03-19 19:35:51 +0200

    hash tag+image handling

--- app/src/main/java/com/studio4us/tamar/sneakpeek2/Upload.java
+++ app/src/main/java/com/studio4us/tamar/sneakpeek2/Upload.java
@@ -1,8 +1,16 @@
 package com.studio4us.tamar.sneakpeek2;
 
+import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,12 +18,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 
+import java.io.ByteArrayOutputStream;
+
 // In this case, the fragment displays simple text based on the page
-public class Upload extends Fragment implements View.OnClickListener {
+public class Upload extends Fragment  implements View.OnClickListener {
     //content of the tip
     EditText content;
     String tip;
@@ -28,6 +41,10 @@ public class Upload extends Fragment implements View.OnClickListener {
     EditText tags;
     String tagsString;
 
+    //image to uploade
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload;
+    String imgDecodableString;
     Button upload;
 
     public static Upload newInstance() {
@@ -41,6 +58,7 @@ public class Upload extends Fragment implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     // Inflate the fragment layout we defined above for this fragment
@@ -52,9 +70,17 @@ public class Upload extends Fragment implements View.OnClickListener {
         company = (EditText) view.findViewById(R.id.company);
         tags = (EditText) view.findViewById(R.id.tags);
         upload = (Button) view.findViewById(R.id.button);
+        imageToUpload = (ImageView) view.findViewById(R.id.imageToUpload);
+
+        imageToUpload.setOnClickListener(this);
         upload.setOnClickListener(this);
+
+
+
         return view;
     }
+
+
 
     @Override
     public void onClick(View v) {
@@ -62,11 +88,67 @@ public class Upload extends Fragment implements View.OnClickListener {
         name = company.getText().toString();
         tagsString = tags.getText().toString();
 
+        ParseFile imgFile = null;
+
+        byte[] data = imageToUpload.toString().getBytes();
+        ParseFile imageFile = new ParseFile("img_selected", data);
+        imageFile.saveInBackground();
+
         ParseObject tipObject = new ParseObject("Tip");
         tipObject.put("TipContent", tip);
         tipObject.put("CompanyName", name);
         tipObject.put("Tags", tagsString);
         tipObject.put("Likes", 0);
+        tipObject.put("Image", imageFile);
         tipObject.saveInBackground();
+
+        //handling the img to upload
+        switch (v.getId()){
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+            case R.id.button:
+
+                break;
+
+        }
+    }
+
+
+    public void onActivityResult(Context main, int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+//            imageToUpload.setImageURI(selectedImage);
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = main.getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            imageToUpload.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//
+//
+//            // Get the cursor
+//            Cursor cursor = getContentResolver().query(selectedImage,
+//                    filePathColumn, null, null, null);
+//            // Move to first row
+//            cursor.moveToFirst();
+//
+//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//            imgDecodableString = cursor.getString(columnIndex);
+//            cursor.close();
+//
+//            // Set the Image in ImageView after decoding the String
+//            imageToUpload.setImageBitmap(BitmapFactory
+//                    .decodeFile(imgDecodableString));
+        }
     }
 }
